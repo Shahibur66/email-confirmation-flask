9113ce13849d95130d99a196cbc8b25cd9861cde
Shahibur66 <kokings500@gmail.com>
2020-09-19 17:48:35 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Flask email confirmation 
+
+Confirmation link from gmail using flask mail
+
+
+### Usage
+
+1. Installation
+    * `pip install -r requirements.txt`
+
+
+2. Configuration 
+    * `MAIL_SERVER='smtp.gmail.com'` # mail server default gmail
+    * `MAIL_USERNAME='your mail server username'`
+    * `MAIL_PASSWORD='your mail server password'`
+    * `MAIL_PORT=465`
+    * `MAIL_USE_SSL=True`
+    * `MAIL_USE_TLS=False`    
+
+3. Running
+    * `python app.py`
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, url_for,render_template,request, redirect,flash,session
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+app.config['SECRET_KEY']='YourSecretKey'
+mail = Mail(app)
+
+urlSTS = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+
+@app.route('/',methods=["GET", "POST"])
+def index():
+
+	if request.method == 'POST':
+		recipient=request.form.get('email')
+		print('email:',recipient)
+		sender='windowsking55@gmail.com'
+		token = urlSTS.dumps(recipient, salt='email-confirm')
+		msg = Message('Confirm Email', sender=sender, recipients=[recipient])
+		link = url_for('confirm_email', token=token, _external=True)
+		msg.body = 'Your Email confirmation link {}'.format(link)
+		mail.send(msg)
+		flash(f'Email confirmation link sent your email', 'success')
+		return redirect(url_for('index'))
+	return render_template('index.html')
+
+
+
+@app.route('/token/<token>', methods=["GET", "POST"])
+def confirm_email(token):
+    try:
+        email = urlSTS.loads(token, salt="email-confirm", max_age=86400)
+    except:
+        abort(404)
+    flash(f'Thanks for confirming your email', 'success')
+    return redirect(url_for('index'))
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
